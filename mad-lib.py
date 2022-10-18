2beca2d92f3a17af057a64618a5051ccd63dbe3d
caiquemiranda <caiquemirandas@gmail.com>
2022-10-18 19:11:24 -0300

    mad-lib

---
+++ mad-lib.py
@@ -0,0 +1,11 @@
+
+adjective1 = input('Tell me an adjectice, and click enter.')
+noun1 = input('Tell me a noun(plural), and click enter.')
+
+noun2 = input('Tell me another noun, and click enter.')
+adjective2 = input('Tell me a anouther adjectice, and click enter.')
+
+print('Roses are ' + adjective1)
+print(noun1 + 'are blue')
+print(noun2 + ' is ' + adjective2)
+print('And so are you!')
